51db8eabb70e0eeba1ace6a940e65673d0668f27
阿部亮則（AbeAkinori） <akinori.work99@gmail.com>
2021-05-21 15:20:49 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+(()=>{
+'use strict';
+
+let STORAGE_KEY = 'todos-vuejs'
+let todoStorage = {
+  fetch: function () {
+    let todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+new Vue({
+  el: '#app',
+
+  data: {
+    todos: [],
+    current: -1,
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0, label: '作業中' },
+      { value: 1, label: '完了' }
+    ]
+  },
+
+  computed: {
+    computedTodos: function () {
+      return this.todos.filter(function (el) {
+        return this.current < 0 ? true : this.current === el.state
+      }, this)
+    },
+    labels() {
+      return this.options.reduce(function (a, b) {
+        return Object.assign(a, { [b.value]: b.label })
+      }, {})
+    }
+  },
+
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+
+  created() {
+    this.todos = todoStorage.fetch()
+  },
+
+  methods: {
+    doAdd: function(event, value) {
+      let task = this.$refs.task
+      if (!task.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        task: task.value,
+        state: 0
+      })
+      task.value = ''
+    },
+    doChangeState: function (item) {
+      item.state = !item.state ? 1 : 0
+    },
+    doRemove: function (item) {
+      let index = this.todos.indexOf(item)
+      if(confirm('このタスクを削除しますか?')){
+        this.todos.splice(index, 1);
+      }
+    }
+  }
+})
+
+})();
---
+++ js/timer.js
@@ -0,0 +1,108 @@
+(()=>{
+'use strict';
+
+let timer = document.getElementById('timer');
+let min1 = document.getElementById('min1');
+let sec1 = document.getElementById('sec1');
+let min10 = document.getElementById('min10');
+let sec10 = document.getElementById('sec10');
+let reset = document.getElementById('reset');
+let start = document.getElementById('start');
+
+let startTime;
+let timeLeft;
+let timeToCountDown = 0 ;
+let timerId;
+let isRunning = false;
+
+function updateTimer(t){
+  let d = new Date(t);
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  m = ('0' + m).slice(-2);
+  s = ('0' + s).slice(-2);
+  timer.textContent = m + ':' + s;
+  document.title = `残り  ${m + ':' + s}`;
+}
+
+function countDown(){
+  timerId = setTimeout(()=>{
+    timeLeft = timeToCountDown - (Date.now() - startTime);
+    if(timeLeft < 0){
+      isRunning = false;
+      start.textContent = 'スタート';
+      clearTimeout(timerId);
+      timeLeft = 0;
+      timeToCountDown = 0 ;
+      updateTimer(timeLeft);
+      return;
+    }
+    updateTimer(timeLeft);
+    countDown();
+  },10);
+}
+
+start.addEventListener('click',()=>{
+  if(isRunning === false){
+    isRunning = true;
+    start.textContent = 'ストップ';
+    startTime = Date.now();
+    countDown();
+  }else{
+    isRunning = false;
+    start.textContent = 'スタート'
+    timeToCountDown = timeLeft;
+    clearTimeout(timerId);
+  }
+});
+
+min1.addEventListener('click',()=>{
+  if(isRunning === true){
+    return;
+  }
+  timeToCountDown += 60 * 1000;
+  if(timeToCountDown >= 60 * 60 * 1000){
+    timeToCountDown = 0 ;
+  }
+  updateTimer(timeToCountDown);
+});
+
+min10.addEventListener('click',()=>{
+  if(isRunning === true){
+    return;
+  }
+  timeToCountDown += 60 * 1000 * 10;
+  if(timeToCountDown >= 60 * 60 * 1000){
+    timeToCountDown = 0 ;
+  }
+  updateTimer(timeToCountDown);
+});
+
+sec1.addEventListener('click',()=>{
+  if(isRunning === true){
+    return;
+  }
+  timeToCountDown += 1000;
+  if(timeToCountDown >= 60 * 60 * 1000){
+    timeToCountDown = 0 ;
+  }
+  updateTimer(timeToCountDown);
+});
+
+sec10.addEventListener('click',()=>{
+  if(isRunning === true){
+    return;
+  }
+  timeToCountDown += 1000 * 10;
+  if(timeToCountDown >= 60 * 60 * 1000){
+    timeToCountDown = 0 ;
+  }
+  updateTimer(timeToCountDown);
+});
+
+reset.addEventListener('click',()=>{
+  timeToCountDown = 0 ;
+  updateTimer(timeToCountDown);
+});
+
+})();
